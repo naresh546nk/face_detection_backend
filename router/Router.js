a396f63b943f252de5c84b8063b3e03bd72e0aa7
naresh546nk <naresh546.nk@gmail.com>
2021-11-11 08:49:19 +0530

    changed the router url

--- router/Router.js
+++ router/Router.js
@@ -1,11 +1,12 @@
 import {addUser, userLogin, setRank , getRank,getMessage} from '../service/Service.js'
 
 const Router=(app, parser)=>{
+    const url="http://facedetection-backend5466.herokuapp.com"
 
-    app.post("/user", addUser)
-    app.post('/login', userLogin)
-    app.get('/setRank', setRank)
-    app.get('/getRank', getRank)
+    app.post(url+"/user", addUser)
+    app.post(url+'/login', userLogin)
+    app.get(url+'/setRank', setRank)
+    app.get(url+'/getRank', getRank)
     app.get('/', getMessage)
 
 }
