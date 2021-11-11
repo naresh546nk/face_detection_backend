efb2860ee72a2f615c540f6df7a146be832e5377
naresh546nk <naresh546.nk@gmail.com>
2021-11-11 08:06:26 +0530

    databases deployed to cloud

--- env.js
+++ env.js
@@ -2,10 +2,14 @@ import mysql from 'mysql'
 
 const env={
 	PORT:3001,
-	database:'smart-brain',
-	username:'root',
-	password:'',
-	host:'localhost',
+	// database:'smart-brain',
+	// username:'root',
+	// password:'',
+	// host:'localhost',
+	database:'heroku_01c077c06d98fa1',
+	username:'bffdd00ca2d71d',
+	password:'4e18a2a4',
+	host:'us-cdbr-east-04.cleardb.com',
 	dialect:'mysql',
 	operatorsAliases:false,
 	pool: {
@@ -15,4 +19,6 @@ const env={
 	}
 
 }
+
+//mysql://bffdd00ca2d71d:4e18a2a4@us-cdbr-east-04.cleardb.com/heroku_01c077c06d98fa1?
 export default env;
